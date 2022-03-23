fc91b5114b834bde3f6b4df3c4b8be1fc162b0b0
Brian Mohammed Catraguna <briancatraguna@rocketmail.com>
2022-03-23 15:09:44 +0700

    Added code for viewmodel

--- app/src/main/java/com/briancatraguna/viewmodelpractice/MainActivity.kt
+++ app/src/main/java/com/briancatraguna/viewmodelpractice/MainActivity.kt
@@ -4,52 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.briancatraguna.viewmodelpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var tvCount: TextView
-    private lateinit var btnPlus: Button
-    private lateinit var btnMinus: Button
+    private lateinit var binding: ActivityMainBinding
 
-    /**
-     * Count state
-     */
-    private var count = STARTING_NUMBER
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        /**
-         * Initialize count
-         */
-        tvCount = findViewById(R.id.txt_count)
-        tvCount.text = count.toString()
-
-        /**
-         * Initialize buttons
-         */
-        btnPlus = findViewById(R.id.btn_plus)
-        btnMinus = findViewById(R.id.btn_minus)
-
-        /**
-         * Adds the count
-         */
-        btnPlus.setOnClickListener {
-            count += 1
-            tvCount.text = count.toString()
+        binding.apply {
+            vm = viewModel
+            lifecycleOwner = this@MainActivity
         }
 
-        /**
-         * Minuses the count
-         */
-        btnMinus.setOnClickListener {
-            count -= 1
-            tvCount.text = count.toString()
-        }
     }
 
-    companion object {
-        const val STARTING_NUMBER = 0
-    }
 }
---
+++ app/src/main/java/com/briancatraguna/viewmodelpractice/MainViewModel.kt
@@ -0,0 +1,20 @@
+package com.briancatraguna.viewmodelpractice
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    private val _count = MutableLiveData(0)
+    val count: LiveData<Int>
+        get() = _count
+
+    fun increaseCount() {
+        _count.value = _count.value?.plus(1)
+    }
+
+    fun decreaseCount() {
+        _count.value = _count.value?.minus(1)
+    }
+}
